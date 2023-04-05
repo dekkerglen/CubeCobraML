25b6b9859b3969f57df04966ee70c43a17acc2f2
dekkerglen <dekker.glen@gmail.com>
2023-04-05 18:46:21 -0400

    Update ml.js

--- demo/server/ml.js
+++ demo/server/ml.js
@@ -1,6 +1,7 @@
 const tf = require('@tensorflow/tfjs-node');
+const fs = require('fs');
 
-const inbdexToOracle = JSON.parse(fs.readFileSync("indexToOracleMap.json"));
+const indexToOracle = JSON.parse(fs.readFileSync("indexToOracleMap.json"));
 const oracleToIndex = Object.fromEntries(
   Object.entries(indexToOracle).map(([key, value]) => [value, key])
 );
@@ -8,14 +9,18 @@ const oracleToIndex = Object.fromEntries(
 let encoder;
 let recommend_decoder;
 
-tf.loadLayersModel('file://../../model/model/encoder/model').then((model) => {
+tf.loadLayersModel('file://../../model/model/encoder_js/model/model.json').then((model) => {
   encoder = model;
   console.log('encoder loaded');
+}).catch((err) => {
+  console.log(err);
 });
 
-tf.loadLayersModel('file://../../model/model/recommend_decoder/model').then((model) => {
+tf.loadLayersModel('file://../../model/model/cube_decoder_js/model/model.json').then((model) => {
   recommend_decoder = model;
   console.log('recommend_decoder loaded');
+}).catch((err) => {
+  console.log(err);
 });
 
 const recommend = (oracles) => {
@@ -33,7 +38,7 @@ const recommend = (oracles) => {
   const recommendations = recommend_decoder.predict(encoded);
 
   const sorted = recommendations.dataSync().map((rating, index) => ({
-    oracle: inbdexToOracle[index],
+    oracle: indexToOracle[index],
     rating
   })).sort((a, b) => b.rating - a.rating);
 
