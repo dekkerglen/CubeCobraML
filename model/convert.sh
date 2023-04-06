d239cfe4f72a601520e3a8ffb98355a8f9afb5c8
dekkerglen <dekker.glen@gmail.com>
2023-04-05 20:29:46 -0400

    Create convert.sh

---
+++ model/convert.sh
@@ -0,0 +1,3 @@
+tensorflowjs_converter ./model/model/encoder/model ./model/tfjs_model/encoder
+tensorflowjs_converter ./model/model/cube_decoder/model ./model/tfjs_model/cube_decoder
+tensorflowjs_converter ./model/model/deck_build_decoder/model ./model/tfjs_model/deck_build_decoder
