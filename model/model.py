6223f2516a7548e4d799dc1e5ccb9b2b4f6b2bbf
dekkerglen <dekker.glen@gmail.com>
2023-04-06 15:52:58 -0400

    asdf

--- model/model.py
+++ model/model.py
@@ -80,7 +80,8 @@ class CubeCobraMLSystem(Model):
     def draft(self, packs, pools, training=None):
         embedding = self.encoder(pools, training=training)
         best_possible_picks = self.draft_decoder(embedding, training=training)
-        return tf.nn.sigmoid(best_possible_picks * self.elos) * packs
+        mask = 1e9 * (1 - packs)
+        return tf.nn.softmax(best_possible_picks * packs * self.elos - mask)
 
     def save_weights(self, filename):
         self.encoder.save_weights(os.path.join(filename, "encoder", 'model'))
